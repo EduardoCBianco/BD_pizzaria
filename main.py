8435d70b0acd11fc63f56888c94cce69adfda90a
Eduardo Crevelaro Bianco <eduardo.crevelaro@agres.com.br>
2023-02-17 19:59:10 -0300

    funcoes esqueleto para cadastro do cliente

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import time
 
+class Cliente:
+    def __init__(self):
+        pass
+    
+    @property
+    def cpf(self):
+        return self.cpf
+    
+    @cpf.setter
+    def set_cpf(self, in_cpf):
+        self.cpf = in_cpf
+
+    @property
+    def nome(self):
+        return self.nome
+    
+    @nome.setter
+    def set_nome(self, in_nome):
+        self.nome = in_nome
+
+    @property
+    def email(self):
+        return self.email
+    
+    @email.setter
+    def set_email(self, in_email):
+        self.email = in_email
+
+
+
 def tela_inicial():
     print("\n E.M. Pizzaria")
     print("\n\t 1 - Fazer Cadastro")
@@ -28,13 +58,15 @@ def trocar_tela(opcao):
             
             
 def tela_login():
-    in_nome = input()
+    print("\n Login")
+    print("\n\t CPF:")
+    in_cpf = input()
     #checar nome no banco de dados
     in_senha = input()
     #checar senha no banco de dados
     
 def tela_cadastro():
-    print("\n\t Cadastro de Cliente")
+    print("\n Cadastro de Cliente")
     
     print("\n\t Nome Completo:")
     cadastro_nome()
@@ -63,8 +95,26 @@ def cadastro_email():
     in_email = input()
 
 def cadastro_endereco():
-    in_email = input()
+    receber_cep()
+    receber_nome_rua()
+    receber_numero_local()
+    receber_complemento()
+    receber_descricao()
     
+def receber_cep():
+    pass
+
+def receber_nome_rua():
+    pass
+
+def receber_numero_local():
+    pass
+
+def receber_complemento():
+    pass
+
+def receber_descricao():
+    pass
 
 while True:
     tela_inicial()
